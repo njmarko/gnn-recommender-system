77aaa8a3e9af4714b59db9d94da210b1f71030bf
matija <matija.matovic99@gmail.com>
2023-02-23 18:25:44 +0100

    Added basic GAT model

---
+++ model/model.py
@@ -0,0 +1,17 @@
+import torch
+from torch_geometric.nn import GATConv, Linear, to_hetero
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GATConv((-1, -1), hidden_channels, add_self_loops=False)
+        self.lin1 = Linear(-1, hidden_channels)
+        self.conv2 = GATConv((-1, -1), out_channels, add_self_loops=False)
+        self.lin2 = Linear(-1, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index) + self.lin1(x)
+        x = x.relu()
+        x = self.conv2(x, edge_index) + self.lin2(x)
+        return x
