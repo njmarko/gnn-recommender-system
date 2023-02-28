a1fb6ce0eac373c44832efd805718c52abac889a
njmarko <marko.njegomir@uns.ac.rs>
2023-02-28 23:46:57 +0100

    Added experiment logging with wandb.

--- train/train.py
+++ train/train.py
@@ -7,6 +7,7 @@ from torch_geometric.transforms import RandomLinkSplit, ToUndirected
 
 from model.model import Model
 from data.load_data import read_customers, read_products, create_graph_edges
+import wandb
 
 
 def weighted_mse_loss(pred, target, weight=None):
@@ -95,6 +96,13 @@ def test(model, data):
 
 
 def main(args):
+    wb_run_train = wandb.init(entity=args.entity, project=args.project_name, group=args.group,
+                              # save_code=True, # Pycharm complains about duplicate code fragments
+                              job_type=args.job_type,
+                              tags=args.tags,
+                              name=f'{args.model}_train',
+                              config=args,
+                              )
     graph_data = load_data()
     train_data, val_data, test_data = split_data(graph_data)
 
@@ -115,13 +123,15 @@ def main(args):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    for epoch in range(1, args.no_epochs+1):
+    for epoch in range(1, args.no_epochs + 1):
         loss = train(model, train_data, optimizer, weight)
         train_rmse = test(model, train_data)
         val_rmse = test(model, val_data)
         test_rmse = test(model, test_data)
+        wb_run_train.log({'train_epoch_loss': loss, 'val_epoch_loss': val_rmse})
         print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_rmse:.4f}, '
               f'Val: {val_rmse:.4f}, Test: {test_rmse:.4f}')
+    wb_run_train.finish()
 
 
 if __name__ == '__main__':
@@ -129,5 +139,25 @@ if __name__ == '__main__':
     PARSER.add_argument('--use_weighted_loss', action='store_true',
                         help='Whether to use weighted MSE loss.')
     PARSER.add_argument('--no_epochs', default=300, type=int)
+    # Wandb logging options
+    PARSER.add_argument('-entity', '--entity', type=str, default="weird-ai-yankovic",
+                        help="Name of the team. Multiple projects can exist for the same team.")
+    PARSER.add_argument('-project_name', '--project_name', type=str, default="gnn-recommender-system",
+                        help="Name of the project. Each experiment in the project will be logged separately"
+                             " as a group")
+    PARSER.add_argument('-group', '--group', type=str, default="default_experiment",
+                        help="Name of the experiment group. Each model in the experiment group will be logged "
+                             "separately under a different type.")
+    PARSER.add_argument('-save_model_wandb', '--save_model_wandb', type=bool, default=True,
+                        help="Save best model to wandb run.")
+    PARSER.add_argument('-job_type', '--job_type', type=str, default="train",
+                        help="Job type {train, eval}.")
+    PARSER.add_argument('-tags', '--tags', nargs="*", type=str, default="train",
+                        help="Add a list of tags that describe the run.")
+    # Model options
+    model_choices = ['GRAPH_SAGE']
+    PARSER.add_argument('-m', '--model', type=str.lower, default="GRAPH_SAGE",
+                        choices=model_choices,
+                        help=f"Model to be used for training {model_choices}")
     ARGS = PARSER.parse_args()
     main(ARGS)
